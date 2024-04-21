769e7934f1ec042e4c2326abca9dd6b0b04e3624
Restyled.io <commits@restyled.io>
2024-04-21 17:47:35 +0000

    Restyled by prettier

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import Logo from '../assets/logos/logo-black.svg';
+import Logo from "../assets/logos/logo-black.svg";
 
 const Footer = () => {
   return (
-    <div className='bg-pink'>
-      <div className='grid p-5 lg:px-12 lg:pt-6 max-w-7xl m-auto'>
-        <div className="self-center justify-self-center bg-no-repeat bg-center bg-cover	h-20 w-20 " to="/" style={{backgroundImage: `url(${Logo})`}}></div>
+    <div className="bg-pink">
+      <div className="grid p-5 lg:px-12 lg:pt-6 max-w-7xl m-auto">
+        <div
+          className="self-center justify-self-center bg-no-repeat bg-center bg-cover	h-20 w-20 "
+          to="/"
+          style={{ backgroundImage: `url(${Logo})` }}
+        ></div>
       </div>
     </div>
   );
-}
+};
 
 export default Footer;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import heroImg from '../assets/images/hero.jpg'
+import heroImg from "../assets/images/hero.jpg";
 
 const Hero = () => {
   return (
-    <section className='relative h-96 md:h-screen bg-cover bg-no-repeat bg-center bg-[image:var(--image-url)]' 
-    style={{'--image-url': `url(${heroImg})`}}>
-      <div className=' md:pt-0 pt-10 absolute top-0 left-0 right-0 bottom-0 backdrop-brightness-50 justify-center align-middle flex flex-col'>
-        
-        <h1 className='text-center text-4xl lg:text-6xl font-serif text-white'>DANI<span className='text-7xl lg:text-8xl font-sans'> & </span>LAIA</h1>
-        
-        <p className='text-center pt-4 text-base italic text-pink'>14 Septiembre 2024</p>
+    <section
+      className="relative h-96 md:h-screen bg-cover bg-no-repeat bg-center bg-[image:var(--image-url)]"
+      style={{ "--image-url": `url(${heroImg})` }}
+    >
+      <div className=" md:pt-0 pt-10 absolute top-0 left-0 right-0 bottom-0 backdrop-brightness-50 justify-center align-middle flex flex-col">
+        <h1 className="text-center text-4xl lg:text-6xl font-serif text-white">
+          DANI<span className="text-7xl lg:text-8xl font-sans"> & </span>LAIA
+        </h1>
+
+        <p className="text-center pt-4 text-base italic text-pink">
+          14 Septiembre 2024
+        </p>
       </div>
     </section>
   );
-}
+};
 
 export default Hero;
